bbf6e56ef62cccfa61e3960e6bfbe6d4147cc6e1
ArtlessAvian <ArtlessAvian@gmail.com>
2017-03-25 12:49:21 -0700

    Setup Libgdx

---
+++ desktop/src/com/artlessavian/highlyunresponsive/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.artlessavian.highlyunresponsive.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.artlessavian.highlyunresponsive.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
